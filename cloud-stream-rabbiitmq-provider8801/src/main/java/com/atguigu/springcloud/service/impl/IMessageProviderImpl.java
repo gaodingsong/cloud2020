bfba3e206a736f7752301982a2e6abb8a3daf163
gaodingsong <gds941104>
2021-01-14 10:17:11 +0800

    添加stream

---
+++ cloud-stream-rabbiitmq-provider8801/src/main/java/com/atguigu/springcloud/service/impl/IMessageProviderImpl.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+@EnableBinding(Source.class)// 定义消息的推送广告
+public class IMessageProviderImpl implements IMessageProvider {
+
+    @Resource
+    private MessageChannel output;// 消息发送管道
+
+    @Override
+    public String send() {
+        String serial = UUID.randomUUID().toString();
+        output.send(MessageBuilder.withPayload(serial).build());
+
+        System.out.println("**********"+serial);
+        return null;
+    }
+}
