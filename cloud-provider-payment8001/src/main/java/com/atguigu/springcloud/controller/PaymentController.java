a58791c5b1edbdb2956a78704d5bfb1538d0c572
gaodingsong <gds941104>
2021-01-19 09:31:49 +0800

    添加sleuth

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -87,6 +87,11 @@ public class PaymentController {
 
         return serverPort;
     }
+    @GetMapping("/payment/zipkin")
+    public String paymentZipkin(){
+
+        return "hi,this is zipkin";
+    }
 
 
 }
